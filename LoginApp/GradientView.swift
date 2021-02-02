b752600de409a98c39f9b1229ea0763965f902be
Aleksey Lyubushkin <5841459@gmail.com>
2021-02-02 18:00:12 +0300

    Add gradient at WelcomeScreen.

---
+++ LoginApp/GradientView.swift
@@ -0,0 +1,20 @@
+//
+//  GradientView.swift
+//  LoginApp
+//
+//  Created by Lyubushkin on 02.02.2021.
+//
+
+import UIKit
+
+class GradientView: UIView {
+    override open class var layerClass: AnyClass {
+       return CAGradientLayer.classForCoder()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        let gradientLayer = layer as! CAGradientLayer
+        gradientLayer.colors = [UIColor.systemPink.cgColor, UIColor.blue.cgColor]
+    }
+}
